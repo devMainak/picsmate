2c9faeba70fbdae1fb00e23c479c47291fd0dcf8
devMainak <mrterm404@gmail.com>
2025-02-25 18:54:07 +0530

    feat: auth verification api created

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,13 +1,11 @@
 const User = require("../models/user.model");
-const jwt = require("jsonwebtoken");
 const axios = require("axios");
-const accessToken = require("../services/createAccessToken");
-const { createRefreshToken } = require("../services/createRefreshToken");
+const { createAccessToken } = require("../services/createAccessToken");
 const { setSecureCookie } = require("../services/setSecureCookie");
 
 // Google OAuth implementation
 exports.initiateAuth = (req, res) => {
-  const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:${PORT}/auth/google/callback&response_type=code&scope=openid email profile&access_type=offline&prompt=select_account`;
+  const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:${process.env.PORT}/auth/google/callback&response_type=code&scope=openid email profile&access_type=offline&prompt=select_account`;
   res.redirect(googleAuthUrl);
 };
 
@@ -25,7 +23,7 @@ exports.authCallback = async (req, res) => {
         client_secret: process.env.GOOGLE_CLIENT_SECRET,
         code,
         grant_type: "authorization_code",
-        redirect_uri: `http://localhost:${PORT}/auth/google/callback`,
+        redirect_uri: `http://localhost:${process.env.PORT}/auth/google/callback`,
       },
       {
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -57,9 +55,33 @@ exports.authCallback = async (req, res) => {
     const accessToken = createAccessToken(user);
     setSecureCookie(res, accessToken);
 
-    return res.redirect(`https://localhost:5173/profile`);
+    return res.redirect(`http://localhost:5173/dashboard`);
   } catch (error) {
     console.error("Error during authentication:", error);
     res.status(500).json({ message: "Authentication failed" });
   }
+};
+
+exports.login = async (req, res) => {
+  const { email } = req.body;
+  try {
+    if (!email) {
+      return res.status(400).json({ message: "Credentials missing." });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "No user found" });
+    }
+    const accessToken = createAccessToken(user);
+    setSecureCookie(res, accessToken);
+
+    return res.redirect(`http://localhost:5173/profile`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Authorization Failed." });
+  }
+};
+
+exports.verifyAuth = (req, res) => {
+  res.status(200).json({ message: "Authenticated", user: req.user });
 };
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { initiateAuth, authCallback } = require("../controller/auth.controller");
+const {
+  initiateAuth,
+  authCallback,
+  login,
+  verifyAuth,
+} = require("../controller/auth.controller");
+const authenticate = require("../middlewares/auth.middleware");
 
 router.get("/google", initiateAuth);
 router.get("/google/callback", authCallback);
+router.post("/login", login);
+router.get("/verify", authenticate, verifyAuth);
 
 module.exports = router;
