b4f3f718d654976c42279952039b982cf64db583
devMainak <mrterm404@gmail.com>
2025-03-10 13:27:07 +0530

    feat: albumview added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/LoginPage";
 import Photos from "./pages/PhotoView";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import SidebarLayout from "./components/sidebar/SidebarLayout";
+import AlbumView from "./pages/AlbumView";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route element={<ProtectedRoute />}>
             <Route element={<SidebarLayout />}>
               <Route path="/photos" element={<Photos />} />
+              <Route path="/albums" element={<AlbumView />} />
             </Route>
           </Route>
         </Routes>
---
+++ frontend/src/pages/AlbumView.jsx
@@ -0,0 +1,11 @@
+const AlbumView = () => {
+  return (
+    <div>
+      <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        Albums
+      </h3>
+    </div>
+  );
+};
+
+export default AlbumView;
