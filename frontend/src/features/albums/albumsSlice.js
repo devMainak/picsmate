f2fb86c7a5dd19cfdf2e8f7e0dbc96c3db2eb6bd
devMainak <mrterm404@gmail.com>
2025-03-10 15:33:39 +0530

    feat: shareAlbum thunk func

--- frontend/src/features/albums/albumsSlice.js
+++ frontend/src/features/albums/albumsSlice.js
@@ -8,7 +8,65 @@ export const fetchAlbumsAsync = createAsyncThunk(
       const response = await webServerAxios.get("/albums");
       return response.data;
     } catch (error) {
-      return rejectWithValue(error.response.data?.message);
+      return rejectWithValue(
+        error.response.data?.message ||
+          error.message ||
+          "Failed to fetch albums."
+      );
+    }
+  }
+);
+
+export const createAlbumAsync = createAsyncThunk(
+  "album/create",
+  async (album, { rejectWithValue }) => {
+    try {
+      const response = await webServerAxios.post("/albums", {
+        albumdata: album,
+      });
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response.data?.message ||
+          error.message ||
+          "Failed to create album."
+      );
+    }
+  }
+);
+
+export const updateAlbumAsync = createAsyncThunk(
+  "album/update",
+  async (album, { rejectWithValue }) => {
+    try {
+      const response = await webServerAxios.put(`/albums/${album._id}`, {
+        albumdata: album,
+      });
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response.data?.message ||
+          error.message ||
+          "Failed to update album."
+      );
+    }
+  }
+);
+
+export const shareAlbumAsync = createAsyncThunk(
+  "album/share",
+  async ({ albumId, emails }, { rejectWithValue }) => {
+    try {
+      const response = await webServerAxios.post(`/albums/${albumId}/share`, {
+        emails,
+      });
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response.data?.message ||
+          error.message ||
+          "Failed to share album."
+      );
     }
   }
 );
@@ -32,6 +90,21 @@ const albumsSlice = createSlice({
     builder.addCase(fetchAlbumsAsync.rejected, (state, action) => {
       state.loading = false;
       state.error = action.payload.message || "Failed to fetch albums";
+    });
+    builder.addCase(createAlbumAsync.fulfilled, (state, action) => {
+      state.albums.push(action.payload.album);
+    });
+    builder.addCase(updateAlbumAsync.fulfilled, (state, action) => {
+      const { updatedAlbum } = action.payload;
+      state.albums = state.albums.map((album) =>
+        album._id === updatedAlbum._id ? updatedAlbum : album
+      );
+    });
+    builder.addCase(shareAlbumAsync.fulfilled, (state, action) => {
+      const { updatedAlbum } = action.payload;
+      state.albums = state.albums.map((album) =>
+        album._id === updatedAlbum._id ? updatedAlbum : album
+      );
     });
   },
 });
