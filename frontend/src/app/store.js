f77e40df90998115777b7a22ed479bb0fa2d66f4
devMainak <mrterm404@gmail.com>
2025-03-22 11:59:09 +0530

    feat: imageSlice created

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -3,16 +3,18 @@ import storage from "redux-persist/lib/storage";
 import { persistStore, persistReducer } from "redux-persist";
 import authReducer from "../features/auth/authSlice";
 import albumsReducer from "../features/albums/albumsSlice";
+import imagesReducer from "../features/images/imagesSlice";
 
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["auth", "albums"],
+  whitelist: ["auth", "albums", "images"],
 };
 
 const rootReducer = combineReducers({
   auth: authReducer,
   albums: albumsReducer,
+  images: imagesReducer,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
---
+++ frontend/src/features/images/imagesSlice.js
@@ -0,0 +1,164 @@
+import { webServerAxios } from "@/services/axios.lib";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchImagesAsync = createAsyncThunk(
+  "fetch/images",
+  async (albumId, { rejectWithValue }) => {
+    try {
+      const response = await webServerAxios.get(`/albums/${albumId}/images`);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response.data?.message ||
+          error.message ||
+          "Failed to fetch images."
+      );
+    }
+  }
+);
+
+export const fetchImagesByTag = createAsyncThunk(
+  "images/fetchByTag",
+  async ({ albumId, tags }, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(
+        `/api/images/${albumId}?tags=${tags.join(",")}`
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response?.data?.message || "Failed to load images."
+      );
+    }
+  }
+);
+
+export const uploadImageAsync = createAsyncThunk(
+  "upload/image",
+  async ({ formData, albumId }, { rejectWithValue }) => {
+    try {
+      const response = await webServerAxios.post(
+        `/albums/${albumId}/images`,
+        formData,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response.data?.message ||
+          error.message ||
+          "Failed to create image."
+      );
+    }
+  }
+);
+
+export const addFavouriteImageAsync = createAsyncThunk(
+  "favourite/image",
+  async ({ imageId, albumId }, { rejectWithValue }) => {
+    try {
+      const response = await webServerAxios.put(
+        `/albums/${albumId}/images/${imageId}/favourite`
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response.data?.message ||
+          error.message ||
+          "Failed to change favourite status of image."
+      );
+    }
+  }
+);
+
+export const addCommentAsync = createAsyncThunk(
+  "comment/image",
+  async ({ albumId, comment, imageId }, { rejectWithValue }) => {
+    try {
+      const response = await webServerAxios.post(
+        `/albums/${albumId}/images/${imageId}/comments`,
+        {
+          comment,
+        }
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response.data?.message ||
+          error.message ||
+          "Failed to share album."
+      );
+    }
+  }
+);
+
+export const deleteImageAsync = createAsyncThunk(
+  "delete/image",
+  async ({ albumId, imageId }, { rejectWithValue }) => {
+    try {
+      const response = await webServerAxios.delete(
+        `/albums/${albumId}/images/${imageId}`
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(
+        error.response.data?.message ||
+          error.message ||
+          "Failed to delete album."
+      );
+    }
+  }
+);
+
+const imagesSlice = createSlice({
+  name: "images",
+  initialState: {
+    images: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    // fetchAlbumAsync promise cases
+    builder.addCase(fetchImagesAsync.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchImagesAsync.fulfilled, (state, action) => {
+      state.loading = false;
+      state.images = action.payload.images;
+    });
+    builder.addCase(fetchImagesAsync.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload.message || "Failed to fetch albums";
+    });
+    // uploadImage promise cases
+    builder.addCase(uploadImageAsync.fulfilled, (state, action) => {
+      state.images.push(action.payload.savedImage);
+    });
+    // addFavouriteImageAsync promise cases
+    builder.addCase(addFavouriteImageAsync.fulfilled, (state, action) => {
+      const { updatedImage } = action.payload;
+      state.images = state.images.map((image) =>
+        image._id === updatedImage._id ? updatedImage : image
+      );
+    });
+    // addCommentAsync promise cases
+    builder.addCase(addCommentAsync.fulfilled, (state, action) => {
+      const { updatedImage } = action.payload;
+      state.images = state.images.map((image) =>
+        image._id === updatedImage._id ? updatedImage : image
+      );
+    });
+    // deleteImageAsync promise cases
+    builder.addCase(deleteImageAsync.fulfilled, (state, action) => {
+      const { deletedImage } = action.payload;
+      state.images = state.images.filter(
+        (image) => image._id !== deletedImage._id
+      );
+    });
+  },
+});
+
+export default imagesSlice.reducer;
