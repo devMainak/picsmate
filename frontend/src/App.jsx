597770ba5d9455c8a5c02e7bdce63f882bc0da61
devMainak <mrterm404@gmail.com>
2025-03-22 11:58:36 +0530

    feat: new route album details

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Photos from "./pages/photos/PhotoView";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import SidebarLayout from "./components/sidebar/SidebarLayout";
 import AlbumView from "./pages/albums/AlbumView";
+import AlbumDetails from "./pages/albums/AlbumDetails";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Route element={<SidebarLayout />}>
               <Route path="/photos" element={<Photos />} />
               <Route path="/albums" element={<AlbumView />} />
+              <Route path="/albums/:albumId" element={<AlbumDetails />} />
             </Route>
           </Route>
         </Routes>
