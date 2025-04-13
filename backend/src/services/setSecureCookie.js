19aeee3d0f0ff013b513e9b404b3929284874735
devMainak <mrterm404@gmail.com>
2025-04-13 12:35:33 +0530

    fix: cookies issue for https

--- backend/src/services/setSecureCookie.js
+++ backend/src/services/setSecureCookie.js
@@ -1,6 +1,8 @@
 const setSecureCookie = (res, token) => {
   res.cookie("access_token", token, {
     httpOnly: true,
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
